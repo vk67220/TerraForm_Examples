22a5e89fa9a00dba1b233c498badc32d48549201
Varaprasad Kasturi <vk67220@corp.cyient.com>
2026-02-27 14:14:10 +0530

    added

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "my-terraform-state-us-east-1"   # Must already exist
+    bucket         = "my-terra-state-us-east-1"   # Must already exist
     key            = "s3-project/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terra-locks"                    # Must already exist
